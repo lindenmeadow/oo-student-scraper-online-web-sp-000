d050b968cdd149cd7e48f5696f9cde19ef540222
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-19 19:24:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = []
   end
 
   def self.scrape_profile_page(profile_url)
