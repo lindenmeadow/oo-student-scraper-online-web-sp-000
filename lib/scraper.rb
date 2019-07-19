0c2220852ee93e8e40183c56b0ff6c677adae000
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-19 19:26:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,12 @@ class Scraper
 
     page.css("div.student-card").each do |student|
       name = student.css(".student-name").text
-
+      location = student.css("a").attribute("href").value
+      student_info = {
+        :name => name,
+        :location => location,
+        :profile_url => profile_url
+      }
   end
 
   def self.scrape_profile_page(profile_url)
